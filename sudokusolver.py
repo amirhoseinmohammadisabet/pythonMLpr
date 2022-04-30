cf1fa435269c80762d7a62de6a28f0c65419fdfa
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-01 02:21:18 +0430

    proceeding to sudoku

--- sudokusolver.py
+++ sudokusolver.py
@@ -39,4 +39,21 @@ def valid(bo, num, pos):
 
     return True
 
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+
+
 
