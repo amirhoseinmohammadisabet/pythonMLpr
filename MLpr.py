3a6c0130fb16350c92dd9348d566e7e38628adb2
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-04-30 13:03:46 +0430

    first commit on noise reduction

--- MLpr.py
+++ MLpr.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage
-from PIL import Image
-from skimage import data
 
-image = Image.open("gerd.jpg")
+from skimage import data, io
+
+img = data.coffee()
+io.imsave('coffee.png', img)
 
 
