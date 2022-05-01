a5b63b748afdbf95bebec8852961500704785e6c
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-01 12:01:13 +0430

    proceeding to sudoku

--- sudokusolver.py
+++ sudokusolver.py
@@ -54,6 +54,12 @@ def print_board(bo):
                 print(str(bo[i][j]) + " ", end="")
 
 
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
 
+    return None
 
 
