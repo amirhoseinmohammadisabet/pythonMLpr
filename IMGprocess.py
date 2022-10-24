2a43dadee49dcef7f6209aeff32b05d4aa2fbf36
Amirhosein Mohammadisabet <78011140+amirhoseinmohammadisabet@users.noreply.github.com>
2022-10-24 23:50:48 +0330

    Update IMGprocess.py

--- IMGprocess.py
+++ IMGprocess.py
@@ -36,7 +36,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=10)
 
 
 model.evaluate(x_test,  y_test, verbose=2)
